2dc1f651671010ff54d6318cadc988991838bd89
Philipp Leherbauer <p.leherbauer@gmail.com>
2024-06-23 18:12:23 +0200

    Running version added
    "resolutions": { "@types/react": "18.2.0" },
    updated     "drizzle-kit": "^0.22.7",
    and added     "twrnc": "^4.3.0", for type safety

--- app/index.tsx
+++ app/index.tsx
@@ -8,7 +8,7 @@ import {ThemeToggle} from "@/components/ThemeToggle";
 import {Button} from "@/components/ui/button";
 import {useLiveQuery} from "drizzle-orm/expo-sqlite";
 import {useTheme} from "next-themes";
-
+import tw from 'twrnc';
 import {Progress} from "@/components/ui/progress";
 import {Text} from "@/components/ui/text";
 import {habitTable} from "@/db/schema";
@@ -16,97 +16,92 @@ import {Plus, PlusCircle} from "@/components/Icons";
 import {useMigrationHelper} from "@/db/drizzle";
 import {useDatabase} from "@/db/provider";
 import {SettingsIcon} from "lucide-react-native";
-
-
 import {HabitCard} from "@/components/habit";
 import type {Habit} from "@/lib/storage";
 
 export default function Screen() {
-  const {success, error} = useMigrationHelper();
+    const {success, error} = useMigrationHelper();
 
-  if (error) {
-    return (
-      <View className="flex-1 gap-5 p-6 bg-secondary/30">
-        <Text>Migration error: {error.message}</Text>
-      </View>
-    );
-  }
-  if (!success) {
-    return (
-      <View className="flex-1 gap-5 p-6 bg-secondary/30">
-        <Text>Migration is in progress...</Text>
-      </View>
-    );
-  }
+    if (error) {
+        return (
+            <View style={tw`flex-1 gap-5 p-6 bg-secondary/30`}>
+                <Text>Migration error: {error.message}</Text>
+            </View>
+            );
+        }
 
-  return <ScreenContent />;
+    if (!success) {
+        return (
+            <View style={tw`flex-1 gap-5 p-6 bg-secondary/30`}>
+                <Text>Migration is in progress...</Text>
+            </View>
+        );
+    }
+
+    return <ScreenContent />;
 }
 
 function ScreenContent() {
-  const {db} = useDatabase();
-  const {data: habits, error} = useLiveQuery(db?.select().from(habitTable));
+    const {db} = useDatabase();
+    const {data: habits, error} = useLiveQuery(db?.select().from(habitTable));
 
-  const ref = React.useRef(null);
-  useScrollToTop(ref);
+    const ref = React.useRef(null);
+    useScrollToTop(ref);
 
-
-  const renderItem = React.useCallback(
-    ({item}: {item: Habit}) => <HabitCard {...item} />,
-    [],
-  );
-
-  if (error) {
-    return (
-      <View className="flex-1 items-center justify-center bg-secondary/30">
-        <Text className="text-destructive pb-2 ">Error Loading data</Text>
-      </View>
+    const renderItem = React.useCallback(
+        ({item}: {item: Habit}) => <HabitCard {...item} />,
+        [],
     );
-  }
-  return (
-    <View className="flex flex-col basis-full bg-background  p-8">
 
-
-      <Stack.Screen
-        options={{
-          title: "Habits",
-          headerRight: () => <ThemeToggle />,
-        }}
-      />
-      <FlashList
-        ref={ref}
-        className="native:overflow-hidden rounded-t-lg"
-        estimatedItemSize={49}
-        showsVerticalScrollIndicator={false}
-        ListEmptyComponent={() => (
-          <View>
-            <Text className="text-lg">Hi There 👋</Text>
-            <Text className="text-sm">
-              This example use sql.js on Web and expo/sqlite on native
-            </Text>
-            <Text className="text-sm">
-              If you change the schema, you need to run{" "}
-              <Text className="text-sm font-mono text-muted-foreground bg-muted">
-                bun migrate
-              </Text>
-            </Text>
-          </View>
-        )
-        }
-        ItemSeparatorComponent={() => <View className="p-2" />}
-        data={habits}
-        renderItem={renderItem}
-        keyExtractor={(_, index) => `item-${ index }`}
-        ListFooterComponent={< View className="py-4" />}
-      />
-      <View className="absolute web:bottom-20 bottom-10 right-8" >
-        <Link href="/create" asChild>
-          <Pressable>
-            <View className="bg-primary justify-center rounded-full h-[45px] w-[45px]">
-              <Plus className="text-background self-center" />
+    if (error) {
+        return (
+            <View style={tw`flex-1 items-center justify-center bg-secondary/30`}>
+                <Text style={tw`text-destructive pb-2`}>Error Loading data</Text>
             </View>
-          </Pressable>
-        </Link>
-      </View>
-    </View >
-  );
+        );
+    }
+    return (
+        <View style={tw`flex flex-col basis-full bg-background p-8`}>
+            <Stack.Screen
+                options={{
+                    title: "Habits",
+                    headerRight: () => <ThemeToggle />,
+                }}
+            />
+            <FlashList
+                ref={ref}
+                contentContainerStyle={tw`native:overflow-hidden rounded-t-lg`}
+                estimatedItemSize={49}
+                showsVerticalScrollIndicator={false}
+                ListEmptyComponent={() => (
+                    <View>
+                        <Text style={tw`text-lg`}>Hi There 👋</Text>
+                        <Text style={tw`text-sm`}>
+                            This example uses sql.js on Web and expo/sqlite on native
+                        </Text>
+                        <Text style={tw`text-sm`}>
+                            If you change the schema, you need to run{" "}
+                            <Text style={tw`text-sm font-mono text-muted-foreground bg-muted`}>
+                                bun migrate
+                            </Text>
+                        </Text>
+                    </View>
+                )}
+                ItemSeparatorComponent={() => <View style={tw`p-2`} />}
+                data={habits}
+                renderItem={renderItem}
+                keyExtractor={(_, index) => `item-${index}`}
+                ListFooterComponent={<View style={tw`py-4`} />}
+            />
+            <View style={tw`absolute web:bottom-20 bottom-10 right-8`}>
+                <Link href="/create" asChild>
+                    <Pressable>
+                        <View style={tw`bg-primary justify-center rounded-full h-[45px] w-[45px]`}>
+                            <Plus style={tw`text-background self-center`} />
+                        </View>
+                    </Pressable>
+                </Link>
+            </View>
+        </View>
+    );
 }
